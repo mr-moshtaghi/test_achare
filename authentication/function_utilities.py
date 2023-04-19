f15cbe3e3182eb73657c967e953965bb2762edd8
ahmad_ali_moshtaghi <moshtaghi@gmail.com>
2023-04-19 16:47:09 +0330

    use cache

--- authentication/function_utilities.py
+++ authentication/function_utilities.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.cache import cache
 from django.utils import timezone
 
 from achare import settings
@@ -13,40 +14,57 @@ def get_client_ip(request):
     return request.META.get('REMOTE_ADDR')
 
 
+def get_key_wrong_attempt(key):
+    return f"wrong_{key}_attempt"
+
+
+def wrong_attempt_object(el):
+    key = get_key_wrong_attempt(el)
+    if not cache.get(key):
+        cache.set(key, 0)
+    result = cache.incr(key, 1)
+
+    if result > 3:
+        cache.delete(key)
+        cache.set(f"ban_{el}", True, 60 * 60 * 60)
+
+
 def wrong_attempt(ip_address, cellphone):
-    ban_cellphone = BanCellphone.objects.filter(
-        cellphone=cellphone,
-        ban_date_time__isnull=True
-    ).first()
-    if not ban_cellphone:
-        ban_cellphone = BanCellphone.objects.create(cellphone=cellphone)
+    wrong_attempt_object(ip_address)
+    wrong_attempt_object(cellphone)
 
-    ban_cellphone.number_of_try = ban_cellphone.number_of_try + 1
-    ban_cellphone.save()
-
-    if ban_cellphone.number_of_try >= settings.NUMBER_OF_TRY_FOR_BAN:
-        ban_cellphone.ban_date_time = timezone.now() + timezone.timedelta(minutes=settings.BAN_TIME_USER)
-        ban_cellphone.save()
-
-    ban_ip = BanIp.objects.filter(
-        ip_address=ip_address,
-        ban_date_time__isnull=True
-    ).first()
-
-    if not ban_ip:
-        ban_ip = BanIp.objects.create(ip_address=ip_address)
-
-    ban_ip.number_of_try = ban_ip.number_of_try + 1
-    ban_ip.save()
-
-    if ban_ip.number_of_try >= settings.NUMBER_OF_TRY_FOR_BAN:
-        ban_ip.ban_date_time = timezone.now() + timezone.timedelta(minutes=settings.BAN_TIME_USER)
-        ban_ip.save()
+    # ban_cellphone = BanCellphone.objects.filter(
+    #     cellphone=cellphone,
+    #     ban_date_time__isnull=True
+    # ).first()
+    # if not ban_cellphone:
+    #     ban_cellphone = BanCellphone.objects.create(cellphone=cellphone)
+    #
+    # ban_cellphone.number_of_try = ban_cellphone.number_of_try + 1
+    # ban_cellphone.save()
+    #
+    # if ban_cellphone.number_of_try >= settings.NUMBER_OF_TRY_FOR_BAN:
+    #     ban_cellphone.ban_date_time = timezone.now() + timezone.timedelta(minutes=settings.BAN_TIME_USER)
+    #     ban_cellphone.save()
+    #
+    # ban_ip = BanIp.objects.filter(
+    #     ip_address=ip_address,
+    #     ban_date_time__isnull=True
+    # ).first()
+    #
+    # if not ban_ip:
+    #     ban_ip = BanIp.objects.create(ip_address=ip_address)
+    #
+    # ban_ip.number_of_try = ban_ip.number_of_try + 1
+    # ban_ip.save()
+    #
+    # if ban_ip.number_of_try >= settings.NUMBER_OF_TRY_FOR_BAN:
+    #     ban_ip.ban_date_time = timezone.now() + timezone.timedelta(minutes=settings.BAN_TIME_USER)
+    #     ban_ip.save()
 
 
 def check_ban_cellphone(cellphone):
     # check cellphone
-    if BanCellphone.objects.filter(cellphone=cellphone, ban_date_time__gte=datetime.datetime.now()).first():
+    if cache.get(f"ban_{cellphone}"):
         return False
     return True
-
--- authentication/permissions.py
+++ authentication/permissions.py
@@ -1,5 +1,6 @@
 import datetime
 
+from django.core.cache import cache
 from rest_framework.permissions import BasePermission
 
 from authentication.function_utilities import get_client_ip
@@ -10,7 +11,11 @@ class IsIPBanned(BasePermission):
     message = 'Your IP has been banned'
 
     def has_permission(self, request, view):
-        return not BanIp.objects.filter(
-                ip_address=get_client_ip(request),
-                ban_date_time__gte=datetime.datetime.now()).first()
+        return not cache.get(f"ban_{get_client_ip(request)}")
+
+        # return not BanIp.objects.filter(
+        #         ip_address=get_client_ip(request),
+        #         ban_date_time__gte=datetime.datetime.now()).first()
+
+
 
